71c3238dcf174ad3f2eedfc5ec0a94c266de98d2
tito-arch <bunniestore.rabbitgroup@gmail.com>
2023-03-13 12:35:39 +0300

    useState hook, conditional classes, css for hidden classes

--- react-image-carousel/src/components/Carousel.jsx
+++ react-image-carousel/src/components/Carousel.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, {useState}from 'react'
 import './carousel.css'
 import {BsArrowLeftCircleFill,BsArrowRightCircleFill} from 'react-icons/bs'
 
 function Carousel({data}) {
   //console.log(data);
+  const [slide, setSlide] = useState(0);
+  const nextSlide= () => {
+    setSlide(slide === data.length -1 ? 0 : slide + 1);
+  };
+  const prevSlide= () => {
+    setSlide(slide === 0 ? data.length -1 : slide - 1);
+  };
   return (
     
     <div className='carousel'>
-      <BsArrowLeftCircleFill  className='arrow arrow-left'/>
+      <BsArrowLeftCircleFill  className='arrow arrow-left' onClick={prevSlide}/>
 
       {data.map((item, idx) => {
-        return <img src={item.src} alt={item.alt} key={idx} className='slide'/>
+        return <img src={item.src} alt={item.alt} key={idx} className={slide === idx ? "slide":"slide slide-hidden"}/>
       })}
       
-      <BsArrowRightCircleFill className='arrow arrow-right' />
+      <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide}/>
 
       <span className='indicators'>
         {
           data.map((_, idx) => {
-            return <button key={idx} onClick={null} className='indicator'></button>
+            return <button key={idx} onClick={() => setSlide(idx)} className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>
           })
         }
       </span>
